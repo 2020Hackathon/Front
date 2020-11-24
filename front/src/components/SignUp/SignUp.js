47980a8deaf88028ea82a068fea84ce2a990a5fb
parkjaehoon <yukihoon12@naver.com>
2020-11-24 17:09:28 +0900

    [CREATE] 로그인 회원가입 구현 완료

--- front/src/components/SignUp/SignUp.js
+++ front/src/components/SignUp/SignUp.js
@@ -53,35 +53,38 @@ const SignUp = () => {
     },
     [userData]
   );
-  const signUp = useCallback(async () => {
-    if (userData.PW === userData.PWcheck) {
-      try {
-        const res = await requestAPI(
-          "/v1/signUp/basic",
-          {
-            name: userData.name,
-            id: userData.ID,
-            password: userData.PW,
-            userinfo: userData.belong,
-          },
-          {},
-          "post"
-        );
-        console.log(res);
-        // if (res.status === 200) {
-        //   alert("회원가입 되었습니다.");
-        //   history.push("./home");
-        // }
-        // else if(res.status === 400){
-        //     alert('이미 있는 아이디입니다.')
-        // }
-      } catch (err) {
-        console.log(err);
+  const signUp = useCallback(
+    async (e) => {
+      console.log(e);
+      e.preventDefault();
+      if (userData.PW === userData.PWcheck) {
+        try {
+          const res = await requestAPI(
+            "/v1/signUp/basic",
+            {
+              name: userData.name,
+              id: userData.ID,
+              password: userData.PW,
+              userinfo: userData.belong,
+            },
+            {},
+            "post"
+          );
+          if (res.status === 200) {
+            alert("회원가입 되었습니다.");
+            history.push("./login");
+          }
+        } catch (err) {
+          if (err === 400) {
+            alert("이미 있는 유저입니다.");
+          }
+        }
+      } else {
+        alert("비밀번호가 서로 다릅니다.");
       }
-    } else {
-      alert("비밀번호가 서로 다릅니다.");
-    }
-  }, [userData]);
+    },
+    [userData]
+  );
   return (
     <S.Container>
       <S.SignUpImg src={signUpImg} />
@@ -89,37 +92,46 @@ const SignUp = () => {
         <S.TitleImg src={logo} />
         <S.LoginTitle>회원가입</S.LoginTitle>
         <S.MarginBlock />
-        <S.Input
-          placeholder="이름"
-          value={userData.name}
-          id="name"
-          onChange={setState}
-        />
-        <S.Input
-          placeholder="아이디"
-          value={userData.ID}
-          id="id"
-          onChange={setState}
-        />
-        <S.Input
-          placeholder="비밀번호"
-          value={userData.PW}
-          id="pw"
-          onChange={setState}
-        />
-        <S.Input
-          placeholder="비밀번호 확인"
-          value={userData.PWcheck}
-          id="checkPW"
-          onChange={setState}
-        />
-        <S.Select id="belong" onChange={setState}>
-          <option>소속(선택)</option>
-          <option>대전</option>
-          <option>대구</option>
-          <option>광주</option>
-        </S.Select>
-        <S.Button onClick={signUp}>회원가입</S.Button>
+        <form onSubmit={signUp}>
+          <S.Input
+            placeholder="이름"
+            value={userData.name}
+            id="name"
+            onChange={setState}
+            required
+          />
+          <S.Input
+            placeholder="아이디"
+            value={userData.ID}
+            id="id"
+            onChange={setState}
+            required
+          />
+          <S.Input
+            placeholder="비밀번호"
+            value={userData.PW}
+            id="pw"
+            onChange={setState}
+            required
+          />
+          <S.Input
+            placeholder="비밀번호 확인"
+            value={userData.PWcheck}
+            id="checkPW"
+            onChange={setState}
+            required
+          />
+          <S.Select id="belong" onChange={setState} required>
+            <option>소속(선택)</option>
+            <option>대전</option>
+            <option>대구</option>
+            <option>광주</option>
+          </S.Select>
+          <S.Button onSubmit={signUp} type="submit">
+            회원가입
+          </S.Button>
+        </form>
+
         <S.GoLogin onClick={goToLogin}>이미 계정이 있으신가요?</S.GoLogin>
       </S.SignUpContainer>
     </S.Container>
